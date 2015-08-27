aaf1b7a76ed250b187d3a84ce2bfee836223f00e
Vladimir Vasilev <vav85@abv.bg>
2015-08-27 12:23:47 +0300

    Update roots.cpp

--- week2/1-Roots/roots.cpp
+++ week2/1-Roots/roots.cpp
@@ -1,8 +1,48 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
 class Roots {
 public:
-
-  // Finds the square root of a number using binary search.
-  double squareRoot(int number) {
-    // ...
+  static double squareRoot(double number) {
+    if(number == 0 || number == 1) {
+            return number;
+        }
+                double start, end;
+                if(number<1){
+           start = number, end = 1;
+                }
+                else{
+         start = 0, end = number+1 ;
+                }
+                double mid;
+        while(end - start >= 0.0000001 ) {
+            mid = start + (end - start) / 2;
+            if(mid == number / mid) {
+                return mid;
+            }
+            if(mid < number / mid) {
+                start = mid ;
+            } else {
+                end = mid ;
+            }
+        }
+        return start;
   }
 };
+
+
+int main()
+{   
+        double number;
+        cin>>number;
+                number = Roots::squareRoot(number);
+                unsigned short significantDecimalFigures = 0;
+                for (long i = (long)number; i; i/=10)
+                {
+                        significantDecimalFigures++;
+                }
+                                printf("%.5f\n",number);
+        //cout<<setprecision(5+significantDecimalFigures)<<number<<endl;      
+        return 0;
+}
