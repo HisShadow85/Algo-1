3f6c6d775cb97e15f6e43b7ebbb23337dc68fdda
Vladimir Vasilev <vav85@abv.bg>
2015-08-27 15:12:40 +0300

    Update min_max_heap.cpp

--- week3/2-Min-Max-Heap/min_max_heap.cpp
+++ week3/2-Min-Max-Heap/min_max_heap.cpp
@@ -1,15 +1,47 @@
-class Node {
-public:
-  int value;
-  Node* left;
-  Node* right;
-};
+#include<iostream>
+#include<stack>
+//#include<fstream>
+using namespace std;
 
-class MinMaxHeap {
-public:
 
-  // Checks if a binary tree is a min/max heap.
-  bool isMinMax(Node* root) {
-    // ...
-  }
-};
+bool isMinMaxHeap(int *tree,unsigned N){
+     unsigned nodeIndex = 1;
+         unsigned nodesInLevel = 1;
+         unsigned maxIndexOfLevel;
+                 for (int level = 1; nodeIndex < N; level++)
+                 {   
+                         nodesInLevel<<=1;
+                         if(nodesInLevel+nodeIndex > N) maxIndexOfLevel = N;
+                         else maxIndexOfLevel = nodesInLevel+nodeIndex;
+                         if(!(level%2)){
+                                 for (; nodeIndex < maxIndexOfLevel ;nodeIndex++)
+                                 {
+                                         unsigned parent = (nodeIndex-1)/2;
+                                         if(tree[nodeIndex++] > tree[parent]) return false;
+                                         if(nodeIndex < maxIndexOfLevel && tree[nodeIndex]> tree[parent]) return false;
+                                 }
+                         }else{
+                                 for (; nodeIndex < maxIndexOfLevel ;nodeIndex++)
+                                 {
+                                         unsigned parent = (nodeIndex-1)/2;
+                                         if(tree[nodeIndex++] < tree[parent]) return false;
+                                         if(nodeIndex < maxIndexOfLevel && tree[nodeIndex] < tree[parent]) return false;
+                                 }
+             }
+                 }
+                 return true;
+}
+
+int main(){
+        //ifstream in("input.txt");
+        int N;
+        cin>>N;
+        int *tree = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+                cin>>tree[i];
+        }
+
+        cout<<(isMinMaxHeap(tree,N)?"YES":"NO")<<endl;
+        return 0;
+}
